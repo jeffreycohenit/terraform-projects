11d5b6439e2d72fd8cffab9371654fdb8d1b69ed
Jeffrey <jeffreycohenit@gmail.com>
2023-01-02 13:54:12 +0000

    Saving my changes

--- Project1/main.tf
+++ Project1/main.tf
@@ -6,7 +6,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "web_server" {
+# First EC2 instance
+resource "aws_instance" "web_server_1" {
   ami                    = "ami-0b5eea76982371e91" # Amazon Linux 2 
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web_server.id]
@@ -14,13 +15,50 @@ resource "aws_instance" "web_server" {
 #!/bin/bash
 yum -y update
 yum -y install httpd
-MYIP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h2>WebServer with PrivateIP: $MYVIP</h2><br>Built by Terraform" > /var/www/html/index.html
-service httpd start
-chkconfig httpd on
+systemctl start httpd
+systemctl enable httpd
+echo "<h2>Apache WebServer 1</h2>" > /var/www/html/index.html
 EOF
   tags = {
-    Name  = "Apache WebServer Terraform"
+    Name  = "1st Apache WebServer Terraform"
+    Owner = "Jeffrey"
+  }
+}
+
+# Second EC2 instance
+resource "aws_instance" "web_server_2" {
+  ami                    = "ami-0b5eea76982371e91" # Amazon Linux 2 
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web_server.id]
+  user_data              = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h2>Apache WebServer 2</h2>" > /var/www/html/index.html
+EOF
+  tags = {
+    Name  = "2nd Apache WebServer Terraform"
+    Owner = "Jeffrey"
+  }
+}
+
+# Third EC2 instance
+resource "aws_instance" "web_server_3" {
+  ami                    = "ami-0b5eea76982371e91" # Amazon Linux 2 
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web_server.id]
+  user_data              = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h2>Apache WebServer 3</h2>" > /var/www/html/index.html
+EOF
+  tags = {
+    Name  = "3rd Apache WebServer Terraform"
     Owner = "Jeffrey"
   }
 }
@@ -29,6 +67,13 @@ resource "aws_security_group" "web_server" {
   name        = "WebServer-SG"
   description = "Security Group for my WebServer"
 
+  ingress {
+    description = "All access on SSH Port" 
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  
   ingress {
     description = "Allow access on HTTP Port"
     from_port   = 80
